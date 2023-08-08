afc3d965969a70d80fd1e5dcb76aa1b48c3ec1dc
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-09 01:30:16 +0300

    8. Remove the Demonstration Module Wrapper Function & Revert the Code Back

--- logger.js
+++ logger.js
@@ -1,18 +1,11 @@
-(function (exports, require, module, __filename, __dirname) {
-  console.log(__filename);
-  console.log(__dirname);
+console.log(__filename);
+console.log(__dirname);
 
-  var url = "http://mylogger.io/log";
+var url = "http://mylogger.io/log";
 
-  function log(message) {
-    // Send an HTTP Request
-    console.log(message);
-  }
+function log(message) {
+  // Send an HTTP Request
+  console.log(message);
+}
 
-  module.exports = log;
-
-  module.exports.log = log;
-  exports.log = log;
-
-  exports = log; // module.exports
-});
+module.exports = log;
