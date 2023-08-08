471e1be0aee361bfe37674073a61905399c62143
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-09 00:41:30 +0300

    2. What is Global Object

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ function sayHello(name) {
 }
 
 sayHello("Fady");
+
+// The variables and functions that we defined here, they are not added to the global object.
+// They are only a scope to this file. app.js, so they are not available outside of this file.
+// And this is because of Node's modular system.
+var message = "";
+console.log(global.message);
