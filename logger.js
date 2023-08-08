7f596ea7071d0adaf10cc944ec63b572cbc94e73
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-09 01:27:11 +0300

    7. What is Module Wrapper Function? (Write it)

--- logger.js
+++ logger.js
@@ -1,8 +1,18 @@
-var url = "http://mylogger.io/log";
+(function (exports, require, module, __filename, __dirname) {
+  console.log(__filename);
+  console.log(__dirname);
 
-function log(message) {
-  // Send an HTTP Request
-  console.log(message);
-}
+  var url = "http://mylogger.io/log";
 
-module.exports = log;
+  function log(message) {
+    // Send an HTTP Request
+    console.log(message);
+  }
+
+  module.exports = log;
+
+  module.exports.log = log;
+  exports.log = log;
+
+  exports = log; // module.exports
+});
