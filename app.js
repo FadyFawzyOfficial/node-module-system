b3b3b7983e8fd7d1930bfdeb32b68b931e4d700d
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-23 15:03:05 +0300

    15. Explore HTTP Module

--- app.js
+++ app.js
@@ -52,3 +52,13 @@ logger.on("messageLogged", (arguments) => {
 });
 
 logger.log("message");
+
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("connection", (socket) => console.log("New Connection"));
+
+server.listen(3000);
+
+console.log("Listening on port 3000 ...");
