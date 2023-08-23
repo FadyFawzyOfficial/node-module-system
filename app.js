1e2291992c3da7ad714bd2f3300c8262e5b918f6
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-23 15:08:53 +0300

    16. Make Simple APIs using Node HTTP Module

--- app.js
+++ app.js
@@ -55,9 +55,20 @@ logger.log("message");
 
 const http = require("http");
 
-const server = http.createServer();
+// const server = http.createServer();
+// server.on("connection", (socket) => console.log("New Connection"));
 
-server.on("connection", (socket) => console.log("New Connection"));
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.write("Hello World");
+    response.end();
+  }
+
+  if (request.url === "/api/courses") {
+    response.write(JSON.stringify([1, 2, 3]));
+    response.end();
+  }
+});
 
 server.listen(3000);
 
